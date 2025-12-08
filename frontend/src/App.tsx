e03eb0f6575a46bf2fda9eea95faf3696ff29154
Rohit Kumar <rohit7091407599@gmail.com>
2025-12-08 11:55:39 +0530

    feat(frontend): Setup vite react app with tailwind css and typescript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-black text-white ">
+      TruEstate 
+    </div>
+  )
+}
+
+export default App
